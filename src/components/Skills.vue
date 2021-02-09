e5c5175e72c70727153146e8320220b0cfc8ee13
Simona <see.moona@yahoo.com>
2021-02-09 22:30:34 +0100

    vue styling

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="hello">
     <div class="holder">
+      <input type="text" placeholder="Add your skill here" v-model="skill" />
       <ul>
-        <li v-for="(data, index) in skills" :key="index">
-          {{ index }}. {{ data.skill }}
-        </li>
+        <li v-for="(data, index) in skills" :key="index">{{ data.skill }}</li>
       </ul>
+      <p>These are the skills you possess:</p>
     </div>
   </div>
 </template>
@@ -15,6 +15,7 @@ export default {
   name: "Skills",
   data() {
     return {
+      skill: "",
       skills: [{ skill: "Vue.js" }, { skill: "Frontend developer" }],
     };
   },
@@ -23,4 +24,40 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.holder {
+  background: #fff;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  border-left: 5px solid #3eb3f6;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+
+p {
+  text-align: center;
+  padding: 30px 0;
+  color: gray;
+}
+
+.container {
+  box-shadow: 0px 0px 40px lightgray;
+}
+input {
+  width: calc(100% - 40px);
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #323333;
+  color: #687f7f;
+}
 </style>
