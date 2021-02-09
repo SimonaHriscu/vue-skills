eb86c139c5debbb49bc7cef33035eeef44ecc13b
Simona <see.moona@yahoo.com>
2021-02-09 21:35:44 +0100

    vue templating

---
+++ src/components/Skills.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    {{ name }}
+    {{ btnState ? "This button is disabled" : " This button is active" }}
+    <button v-on:click="changeName" v-bind:disabled="btnState">
+      Change Name
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+      name: "Coursera",
+      btnState: true,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
